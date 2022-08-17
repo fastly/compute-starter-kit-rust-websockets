6304083d08f0d25efc97fbcf30fa0a66b9ef160a
Justin Karneges <jkarneges@fastly.com>
2022-08-17 11:25:28 -0700

    initial version

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use fastly::experimental::RequestUpgradeWebsocket;
+use fastly::{Error, Request};
+
+fn main() -> Result<(), Error> {
+    let mut req = Request::from_client();
+
+    if let Some("websocket") = req.get_header_str("Upgrade") {
+        Ok(req.upgrade_websocket("backend")?)
+    } else {
+        req.set_pass(true);
+        Ok(req.send("backend")?.send_to_client())
+    }
+}
