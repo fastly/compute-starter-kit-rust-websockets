d9b2b01451654c4e7e196a912ab6668c74fc5f3e
Katsuyuki Omuro <komuro@fastly.com>
2022-10-20 19:11:33 +0900

    Use updated method name handoff_websocket

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Error> {
     let mut req = Request::from_client();
 
     if let Some("websocket") = req.get_header_str("Upgrade") {
-        Ok(req.upgrade_websocket("backend")?)
+        Ok(req.handoff_websocket("backend")?)
     } else {
         req.set_pass(true);
         Ok(req.send("backend")?.send_to_client())
