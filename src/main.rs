fa5fddea091d8d0b6f036548373112bc18a44234
Journie Ma-Johnson <journie.johnson@fastly.com>
2023-02-01 14:34:56 -0800

    Adding service version logging

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use fastly::experimental::RequestUpgradeWebsocket;
 use fastly::{Error, Request};
 
 fn main() -> Result<(), Error> {
+    // Log service version
+    println!("FASTLY_SERVICE_VERSION: {}", std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new()));
+    
     let mut req = Request::from_client();
 
     if let Some("websocket") = req.get_header_str("Upgrade") {
